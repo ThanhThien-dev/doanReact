5b135c954b84a8211386bd848b8a5261242c8ec9
ThanhThien_Dev <tt270797>
2022-05-19 13:52:31 +0700

    fix header

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
 
-        <div className="header-container">
+        <header className="wrapper">
             <div className="header-top">
                 <div className="header-top-inside">
                     <div className="header-logo">
@@ -12,7 +12,7 @@ function Header() {
                 </div>
             </div>
 
-        </div>
+        </header>
     );
 }
 
