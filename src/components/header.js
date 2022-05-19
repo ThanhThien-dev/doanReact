7f1070c83e9b1e1c5183ac877b1e6f56e4a58a26
ThanhThien_Dev <tt270797>
2022-05-20 00:41:43 +0700

    Create header-black

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,24 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
     return (
-
-        <header className="wrapper">
+<header className="container">
+        <div className="wrapper">
 
             {/* HEADER-LOGO */}
             <div className="header-white">
                 <div className="header-logo">
                     <img src={require("./img/logo.png")} className="logo" alt="logo" width="150" height="80" />
-                </div>  
+                </div>
 
+                {/* SEARCH */}
                 <div className="search">
                     <input className="input-search" placeholder="Tìm kiếm" />
+                    <span class="search-span"></span>
                     <button className="search-btn">
-                        <BsSearch  width="52px" height="100%"/>
-                        {/* //import fontawsome icon */}
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </button>
                 </div>
 
@@ -28,10 +30,10 @@ function Header() {
             {/* HEADER-MENU */}
             <div className="header-black">
                 <div className="black-bg">
-                    
+
                 </div>
             </div>
-
+            </div>
         </header>
     );
 }
