fb023f5c95c05ce1e4d4cba6416cc031844599d3
ThanhThien_Dev <tt270797>
2022-06-04 13:37:00 +0700

    nhap main

--- src/components/productDetail.js
+++ src/components/productDetail.js
@@ -11,47 +11,30 @@ function ProductDetail() {
       <section class="section product-detail">
 
         {product.map((thongtin) => {
-          const {id, picture, name, price} = thongtin;
-          return <div key = {id} class="details container">
-          <div class="left">
-            <div class="contain">
-              <div class="main">
-                <img src={require(`./images/hot/${picture}`)} alt="" />
-                
+          const { id, picture, name, price } = thongtin;
+          return <div key={id} class="details container">
+            <div class="left">
+              <div class="contain">
+                <div class="main">
+                  <img src={require(`./images/hot/${picture}`)} alt="" />
+
+                </div>
               </div>
             </div>
+            <div class="right">
 
-            {/* <div class="thumbnails">
-              <div class="thumbnail">
-                <a href="#" data-id="1"><img src="images/img.chi tiết sản phẩm/sp1.jpg" alt="" /></a>
-              </div>
-              <div class="thumbnail">
-                <a href="#" data-id="2"><img src="images/img.chi tiết sản phẩm/sp2.jpg" alt="" /></a>
-              </div>
-              <div class="thumbnail">
-                <a href="#" data-id="3"><img src="images/img.chi tiết sản phẩm/sp3.jpg" alt="" /></a>
-              </div>
-              <div class="thumbnail">
-                <a href="#" data-id="4"><img src="images/img.chi tiết sản phẩm/sp4.jpg" alt="" /></a>
-              </div>
-            </div> */}
+              <h1>{name}</h1>
+              <div class="price">{price}</div>
+
+              <form class="form">
+                <input type="text" placeholder="1" />
+                <Link to={"/giohang"} class="addCart">Thanh toán</Link>
+              </form>
+            </div>
           </div>
-          <div class="right">
-
-            <h1>{name}</h1>
-            <div class="price">{price}</div>
-
-            <form class="form">
-              <input type="text" placeholder="1" />
-              <Link to={"/giohang"} class="addCart">Thanh toán</Link>
-            </form>
-          </div>
-        </div>
         })}
 
-        
       </section>
-
 
       {/* CÁC SẢN PHẨM LIÊN QUAN */}
       <section class="section related-products">
@@ -60,32 +43,32 @@ function ProductDetail() {
           <span>Be exclusive, Be Devine, Be yourself</span>
         </div>
         <div class="product-layout container">
-        {product.map((sanpham) => {
-                    const {id, picture, name, price, hot} = sanpham;
+          {product.map((sanpham) => {
+            const { id, picture, name, price, hot } = sanpham;
 
-                    if(hot === true && id <= 7){
-                    return <div key = {id} className="product">
-                    <div className="img-container">
-                        <img src={require(`./images/hot/${picture}`)} alt="" />
-                        <div className="addCart">
-                            <FontAwesomeIcon icon={faShoppingCart} />
-                        </div>
+            if (hot === true && id <= 7) {
+              return <div key={id} className="product">
+                <div className="img-container">
+                  <img src={require(`./images/hot/${picture}`)} alt="" />
+                  <div className="addCart">
+                    <FontAwesomeIcon icon={faShoppingCart} />
+                  </div>
 
-                        <ul className="side-icons">
-                            <span><FontAwesomeIcon icon={faSearch} /></span>
-                            <span><FontAwesomeIcon icon={faHeart} /></span>
-                            <span><FontAwesomeIcon icon={faSlidersH} /></span>
-                        </ul>
-                    </div>
-                    <div className="bottom">
-                        <a>{name}</a>
-                        <div className="price">
-                            <span>{price}</span>
-                        </div>
-                    </div>
+                  <ul className="side-icons">
+                    <span><FontAwesomeIcon icon={faSearch} /></span>
+                    <span><FontAwesomeIcon icon={faHeart} /></span>
+                    <span><FontAwesomeIcon icon={faSlidersH} /></span>
+                  </ul>
                 </div>
-                }
-                })}
+                <div className="bottom">
+                  <a>{name}</a>
+                  <div className="price">
+                    <span>{price}</span>
+                  </div>
+                </div>
+              </div>
+            }
+          })}
         </div>
       </section>
     </>
