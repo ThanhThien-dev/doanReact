8c4640ace1869ddf8244fd88303fab4c2716649a
phat <lvp3795@gmail.com>
2022-06-04 14:13:42 +0700

    phat fix admin

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,23 +1,21 @@
 import React from "react";
-import './css components/admin.css';
+import "./css components/admin.css";
 
 function Admin() {
-    return (
-        <div className="container">
-            <div className="table">
-                {/* <div className="tittle">QUẢN LÝ SẢN PHẨM</div> */}
-
-                <div className="left">
-                    <h1>left</h1>
-                </div>
-
-                <div className="right">
-                    <h1>right</h1>
-                </div>
-
-            </div>
+  return (
+    <div className="table-admin">
+      <div className="tittle">QUẢN LÝ SẢN PHẨM</div>
+      <div className="table-content">
+        <div className="left">
+          <h1>left</h1>
         </div>
-    );
+
+        <div className="right">
+          <h1>right</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Admin;
