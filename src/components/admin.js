705894e6d5d04ef0f0986a3e2a8bb8f11e525bd0
ThanhThien-dev <tt270797@gmail.com>
2022-06-05 19:06:07 +0700

    done admin

--- src/components/admin.js
+++ src/components/admin.js
@@ -10,35 +10,104 @@ function Admin() {
       <div className="table-content">
         <div className="left">
           <h1>Danh sách sản phẩm</h1>
-          
+
           {product.map((ten) => {
-            const {id, name} = ten;
-            return <div key = {id} className="ds">
+            const { id, name } = ten;
+            return <div key={id} className="ds">
               {name}
             </div>
           })}
-          
+
         </div>
 
-        <div className="right">
-          <h1>Thông tin chi tiết</h1>
-          <div className="header-right">
-          <div className="khungpic">
-          <img className="pic" src={require("./images/hot/1.png")} alt="" />
-          </div>
-          </div>
+        {product.map((ten) => {
+          const { id, picture, brand, name, price, sex, quantity } = ten;
+          if (id === 1) {
+            return <div className="right">
+              <h1>Thông tin chi tiết</h1>
 
-          {/* {product.map((thongtin) => {
-            const {id, picture, brand, name, price, quantity} = thongtin;
+              <div className="header-right">
+                <div className="detail">
+                  <div className="khungpic">
+                    <img className="pic" src={require(`./images/hot/${picture}`)} alt="" />
+                  </div>
+                  <ul key={id} className="detail-chitiet">
+                    <li><b><u>Thương hiệu:</u></b> {brand}</li>
+                    <li><b><u>Tên sản phẩm:</u></b> {name}</li>
+                    <li><b><u>Giá:</u></b> {price}</li>
+                    <li><b><u>Phân loại:</u></b> {sex}</li>
+                    <li><b><u>Số lượng:</u></b> {quantity}</li>
+                  </ul>
+                </div>
 
-            return <div key = {id}>
-              <div className="pic">
-                <img src={require="./images/hot/1.png"} />
-              <img src={require(`./images/hot/${picture}`)} alt="" />
+              </div>
+
+              <div className="bottom-right">
+
+                {/* Màn hình render sửa sản phẩm */}
+                <div className="detail-right">
+                  <ul className="fix-chitiet">
+                    <li><b>Thương hiệu:</b>
+                      <select className="sellect-brand">
+                        <option selected disabled>Chọn thương hiệu</option>
+                        <option>Casio</option>
+                        <option>Citizen</option>
+                        <option>Doxa</option>
+                        <option>Tissot</option>
+                        <option>Seiko</option>
+                        <option>Rolex</option>
+                      </select>
+                    </li>
+                    <li><b>Tên sản phẩm:</b>
+                      <input placeholder={name}></input>
+                    </li>
+                    <li><b>Giá:</b>
+                      <input placeholder={price}></input>
+                    </li>
+                    <li><b>Phân loại:</b>
+                    <select className="sellect-brand">
+                        <option selected disabled>Chọn giới tính</option>
+                        <option>Nam</option>
+                        <option>Nữ</option>
+                      </select>
+                    </li>
+                    <li><b>Số lượng:</b>
+                      <input type={'number'} min={0} placeholder={quantity}></input>
+                    </li>
+                  </ul>
+                </div>
+
+                {/* Màn hình render thêm sản phẩm
+            <div className="detail-right">
+              <ul className="fix-chitiet">
+                <li><b>Thương hiệu:</b>
+                  <input></input>
+                </li>
+                <li><b>Tên sản phẩm:</b>
+                  <input></input>
+                </li>
+                <li><b>Giá:</b>
+                  <input></input>
+                </li>
+                <li><b>Phân loại:</b>
+                  <input></input>
+                </li>
+                <li><b>Số lượng:</b>
+                  <input></input>
+                </li>
+              </ul>
+            </div> */}
+
+                <div className="option">
+                  <button className="add">Thêm sản phẩm</button>
+                  <button className="save">Lưu lại</button>
+                  <button className="del">Xóa sản phẩm</button>
+                </div>
+
               </div>
             </div>
-          })} */}
-        </div>
+          }
+        })}
       </div>
     </div>
   );
